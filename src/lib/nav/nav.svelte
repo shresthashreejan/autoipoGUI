33dc8a4d5718c304113d5c53389a7d8e42af42f1
shreejan shrestha <shr.shreejan@gmail.com>
2024-01-07 12:33:33 +0545

    boilerplate for navbar

---
+++ src/lib/nav/nav.svelte
@@ -0,0 +1,10 @@
+<script>
+	let currentRoute = '/';
+</script>
+
+<nav class="bg-white p-4">
+	<div class="container mx-auto flex justify-end gap-4">
+		<a href="/" class={currentRoute == '/' ? 'font-bold' : ''}>Home</a>
+		<a href="/settings" class={currentRoute == '/settings' ? 'font-bold' : ''}>Settings</a>
+	</div>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
 	import '../app.css';
+	import Nav from '$lib/nav/nav.svelte';
 </script>
 
+<Nav />
 <slot />
